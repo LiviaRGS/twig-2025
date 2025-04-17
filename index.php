1b5bf41ed278325fd59360a42136b6613a4c193c
LiviaRGS <ticorin100@gmail.com>
2025-04-17 10:13:44 -0300

    Campo 'nome' em usuários

--- index.php
+++ index.php
@@ -3,10 +3,14 @@
 require_once('twig_carregar.php');
 session_start();
 $user = $_SESSION['user'] ?? null;
+
 if($user == null){
     header('location:login.php');
 };
 
+$nome = $user['nome'];
+
 echo $twig->render('index.html',[
     'fruta' => 'Merlin',
+    'nome' => $nome,
 ]);
--- register.php
+++ register.php
@@ -4,10 +4,12 @@ require("inc/banco.php");
 
 $us = $_POST['usuario'] ?? null;
 $se = $_POST['senha'] ?? null;
+$no = $_POST['nome'] ?? null;
 
-if($us && $se) {
-    $query = $pdo->prepare('INSERT INTO usuarios (usuario, senha) VALUES (:us, :se)');
+if($us && $se && $no) {
+    $query = $pdo->prepare('INSERT INTO usuarios (usuario, senha, nome) VALUES (:us, :se, :no)');
     $query->bindValue(':us',$us);
+    $query->bindValue(':no',$no);
     $query->bindValue(':se', password_hash($se, PASSWORD_DEFAULT));
     $query->execute();
 
